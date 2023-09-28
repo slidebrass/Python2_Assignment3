af61847850d603d89cae1dd020c38841b8cb2742
slidebrass <slidebrass@gmail.com>
2023-09-28 14:48:23 -0500

    first commit

---
+++ doc3_module.py
@@ -0,0 +1,10 @@
+import math
+
+def findArea(l,w):
+    area = int(l) * int(w)
+    return area
+
+def calcCircum(r):
+    pi = math.pi
+    circumference = 2 * pi * int(r)
+    return circumference
